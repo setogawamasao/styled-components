5752e61e0facea87b8be28ea22f08bbec0bb01ed
masaosetogawa <ms.job.hunting@gmail.com>
2020-06-14 01:51:46 +0900

    Add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const App = () => {
+  const [buttonState, setButtonState] = useState<boolean>(true);
   return (
     <>
-      <Button>hello styled-component!</Button>
+      <Button
+        state={buttonState}
+        onClick={(): void => {
+          setButtonState(!buttonState);
+        }}
+      >
+        hello styled-component!
+      </Button>
     </>
   );
 };
 
-const Button = styled.div`
-  color: red;
+const Button = styled.div<{ state: boolean }>`
+  width: 200px;
+  color: ${(props) => (props.state ? "white" : "")};
+  background: ${(props) => (props.state ? "red" : "")};
 `;
 
 export default App;
